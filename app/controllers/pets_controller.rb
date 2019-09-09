c63b9ab2fb53cf1035fe3d47d3362da3af3d9ac3
Veronica Lopez <nicaa0695@gmail.com>
2019-09-09 17:49:54 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -34,7 +34,18 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-    
+    if !params[:pet].keys.include?("owner_id")
+      params[:pet]["owner_id"] = []
+    end
+    #######
+
+     @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(name: params["owner"]["name"])
+      @owner.pets << @pet
+    end
+    redirect "pets/#{@pet.id}"
 
     redirect to "pets/#{@pet.id}"
   end
